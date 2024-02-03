6c1121dcd48c63fe91899e959bc708d9af342099
aditya <adityapai64@gmail.com>
2024-02-03 23:43:27 +0100

    Until lesson 33

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -7,4 +7,24 @@ import { Component } from "@angular/core";
 export class ServerComponent {
     serverId: number = 10;
     serverStatus: string = "offline"
+    allowNewServer = false;
+    serverCreationStatus: string = "No server was created";
+    serverName: string = ""
+    constructor() {
+        setTimeout(() => {
+            this.allowNewServer = true;
+        }, 2000);
+    }
+
+    ngOnInit() {
+
+    }
+
+    onCreateServer() {
+        this.serverCreationStatus = "Server was created!"
+    }
+
+    onUpdateServerName(event: any) {
+        this.serverName = event.target.value;
+    }
 }
