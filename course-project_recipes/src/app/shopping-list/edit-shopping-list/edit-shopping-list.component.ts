9b9b0223a5bd624a47b025523e00c376028d008b
aditya <adityapai64@gmail.com>
2024-02-12 15:11:17 +0100

    Section 6 complete: Until lecture 92

--- course-project_recipes/src/app/shopping-list/edit-shopping-list/edit-shopping-list.component.ts
+++ course-project_recipes/src/app/shopping-list/edit-shopping-list/edit-shopping-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
+import { Ingredient } from 'src/app/shared/ingredient.model';
 
 @Component({
   selector: 'app-edit-shopping-list',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditShoppingListComponent implements OnInit {
 
+  @ViewChild('nameInput', { static: true }) nameInputRef: ElementRef;
+  @ViewChild('amountInput', { static: true }) amountInputRef: ElementRef;
+
+  @Output() ingredientAdded = new EventEmitter<Ingredient>();
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onAddItem() {
+    const ingredient = new Ingredient(this.nameInputRef.nativeElement.value, this.amountInputRef.nativeElement.value);
+    this.ingredientAdded.emit(ingredient);
+  }
 }
